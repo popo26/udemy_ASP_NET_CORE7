840689359b33457502d3bbb068a21134191aed5d
popo26 <mynameispopo26@gmail.com>
2023-03-23 06:39:12 +1300

    44.Creating Controllers

---
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController //you need to put a word "Controller"
+    {
+        [Route("sayhello")] //attribute routing
+        public string method1()
+        {
+            return "Hello from method1";
+        }
+    }
+}
---
+++ ControllersExample/ControllersExample/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+////to add each controller class manually
+//builder.Services.AddTransient<HomeController>();
+
+// to add all controller classes as service at once
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// this include UseRouting/UseEndpoints/MapControllers all in one.
+app.MapControllers();
+
+////instead of using just MapControllers() 
+//app.UseRouting();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers(); //to detect all controllers as routes
+//});
+
+app.Run();
