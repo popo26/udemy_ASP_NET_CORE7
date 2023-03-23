0b97115fcfe8b8b611065c51608a86f921e27025
popo26 <mynameispopo26@gmail.com>
2023-03-24 06:00:23 +1300

    46.MultipleAction Methods

--- ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -4,10 +4,23 @@ namespace ControllersExample.Controllers
 {
     public class HomeController //you need to put a word "Controller"
     {
-        [Route("sayhello")] //attribute routing
-        public string method1()
+        [Route("/")]
+        [Route("home")]
+        public string Index()
         {
-            return "Hello from method1";
+            return "Hello from Index";
+        }
+
+        [Route("about")] 
+        public string About()
+        {
+            return "Hello from About";
+        }
+
+        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")] //since it is inside {} you need to use // and {{}} inside regex
+        public string Contact()
+        {
+            return "Hello from Comtact";
         }
     }
 }
