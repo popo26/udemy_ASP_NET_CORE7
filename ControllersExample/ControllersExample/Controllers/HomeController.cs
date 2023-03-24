4af1d3aadae1ced1572ddd20e12948c9046deaa5
popo26 <mynameispopo26@gmail.com>
2023-03-25 06:45:43 +1300

    47.ContentResult

--- ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace ControllersExample.Controllers
 {
-    public class HomeController //you need to put a word "Controller"
+    public class HomeController : Controller //you need to put a word "Controller" e.g. HomeController
+
+    //If you don't want to use "Controller" after the class name, use [Controller]
+    //[Controller]
+    //public class Home
+
     {
         [Route("/")]
         [Route("home")]
-        public string Index()
+        public ContentResult Index()
         {
-            return "Hello from Index";
+            //return new ContentResult() { Content = "Hello from Index", 
+            //ContentType = "text/plain"};
+
+            // By adding ":Microsoft.AspNetCore.Mvc.Controller" after HomeController class, you can use this
+            //return Content( "Hello from Index","text/plain");
+
+            //return html
+            return Content("<h1>Welcome</h1> <h2>Hello from Index in HTML</h2>", "text/html");
+          
         }
 
         [Route("about")] 
