c8a7498af6715df8b054caf5229ff472daa0b65f
popo26 <mynameispopo26@gmail.com>
2023-04-05 06:55:34 +1200

    61.Model State

--- ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
@@ -7,8 +7,33 @@ namespace ModelValidationsExample.Controllers
     {
         [Route("register")]
         public IActionResult Index(Person person)
+//With Linq
         {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                
+                return BadRequest(errors);
+            }
             return Content($"{person}");
         }
-    }
+
+//Normal
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        List<string> errorsList = new List<string>();
+        //        foreach (var value in ModelState.Values)
+        //        {
+        //            foreach (var error in value.Errors)
+        //            {
+        //                errorsList.Add(error.ErrorMessage);
+        //            }
+        //        }
+        //        string errors = string.Join("\n", errorsList);
+        //        return BadRequest(errors);
+        //    }
+        //    return Content($"{person}");
+        //}
+    }   
 }
