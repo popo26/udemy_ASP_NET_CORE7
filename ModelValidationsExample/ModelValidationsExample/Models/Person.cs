74c9662f154a2ecd8d80620613f0a1e8ff461a35
popo26 <mynameispopo26@gmail.com>
2023-04-07 08:14:54 +1200

    62/63.All Model Validations Part1/2

--- ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -1,15 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationsExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Person Name can't be empty or null")]
+        [Required(ErrorMessage = "{0} can't be empty or null")]//0 indicate PersonName. Use [Display] when you want to have a different display name.
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength =3, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
+        [RegularExpression("^[A-Za-z .]", ErrorMessage ="{0} should contain only alphabets, space and dot(.).")]
         public string? PersonName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid {0} format.")]
+        [Required(ErrorMessage = "{0} should not be blank.")]
         public string? Email { get; set; }
+
+        [Phone(ErrorMessage = "Invalid {0} format.")]
+        [ValidateNever]//when you want to temporaily exclude from validation
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage ="{0} can't be blank.")]
+        
         public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank.")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match.")]
+        [Display(Name = "Re-enter Password")]
         public string? ConfirmPassword { get; set; }
+
+        [Range(0,999.99, ErrorMessage = "{0} should be between ${1} and ${2}.")]
         public double? Price { get; set; }
 
         public override string ToString()
