b39a6eef713a256817ffdd2bc1d19a4787eb7206
popo26 <mynameispopo26@gmail.com>
2023-04-11 06:45:23 +1200

    69.Input Formatters - xml

--- ModelValidationsExample/ModelValidationsExample/Program.cs
+++ ModelValidationsExample/ModelValidationsExample/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlSerializerFormatters(); //to accept xml from request body into Model objects
 
 var app = builder.Build();
 
