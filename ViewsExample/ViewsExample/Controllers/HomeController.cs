a05abbb6dbe14ef6d1b63962a2e50541c398cfca
popo26 <mynameispopo26@gmail.com>
2023-05-18 06:30:32 +1200

    78.Razor View - If

---
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(); //Views/Home/Index.cshtml
+            //return View("abc"); //abc.cshtml
+            //same as above.
+            //return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}
