c20b0495e1f15ee35840b4d41d7462dcd63a3f37
popo26 <mynameispopo26@gmail.com>
2023-06-04 07:51:33 +1200

    90.Strongly Typed Views - Part2

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -24,5 +24,23 @@ namespace ViewsExample.Controllers
 			return View("Index",people); //Views/Home/Index.cshtml
           
         }
+
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if (name == null)
+                return Content("Person name can't be null");
+
+
+		List<Person> people = new List<Person>()
+	{
+		new Person(){ Name = "John", DateOfBirth=DateTime.Parse("2000-05-06"), PersonGender = Gender.Male},
+		new Person(){ Name = "Linda", DateOfBirth=DateTime.Parse("2005-01-09"), PersonGender = Gender.Female},
+		new Person(){ Name = "Susan", DateOfBirth=null, PersonGender = Gender.Other},
+	};
+
+           Person? matchingPerson =  people.Where(temp => temp.Name == name).FirstOrDefault();
+            return View(matchingPerson); //calling Views/Home/Details.cshtml
+		}
     }
 }
