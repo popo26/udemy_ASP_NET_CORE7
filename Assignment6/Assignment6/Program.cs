a796d5373e418238c8a65aac1b3bc7211fe65d17
popo26 <mynameispopo26@gmail.com>
2023-07-04 06:01:24 +1200

    97.Layout View for Multiple Views

---
+++ Assignment6/Assignment6/Program.cs
@@ -0,0 +1,13 @@
+
+using Assignment6.CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//Invoking custom middleware
+app.UseLoginMiddleware();
+
+app.Run(async context => {
+    await context.Response.WriteAsync("No response");
+});
+app.Run();
