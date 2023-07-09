d5386c9ef9a36de3827bab036d8ca0bc3ed0696a
popo26 <mynameispopo26@gmail.com>
2023-07-10 05:55:00 +1200

    106.Strongly Typed Partial Views

---
+++ PartialViewsExample/PartialViewsExample/Models/ListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PartialViewsExample.Models
+{
+    public class ListModel
+    {
+
+        public string ListTitle { get; set; } = "";
+        public List<string> ListItems { get; set;} = new List<string>();
+    }
+}
