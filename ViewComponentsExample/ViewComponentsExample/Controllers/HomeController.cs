213b9623418ed9e75ddd0d95cd9d78ab26557994
popo26 <mynameispopo26@gmail.com>
2023-07-16 09:23:38 +1200

    114.ViewComponentResult

---
+++ ViewComponentsExample/ViewComponentsExample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("/")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("about")]
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		[Route("friend-list")]
+		public IActionResult LoadFriedsList()
+		{
+			PersonGridModel personGridModel2 = new PersonGridModel()
+			{
+
+				GridTitle = "Friends",
+				Persons = new List<Person>()
+				{
+					new Person(){ PersonName = "John2", JobTitle = "Manager"},
+					new Person(){ PersonName = "Jones3", JobTitle = "Receptionist"},
+					new Person(){ PersonName = "William4", JobTitle = "Cleark"}
+				}
+			};
+
+			return ViewComponent("Grid", new { grid = personGridModel2});
+		}
+	}
+}
