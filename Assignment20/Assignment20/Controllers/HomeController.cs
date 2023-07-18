428be54990ed948249b220142ddbd4854bd43f00
popo26 <mynameispopo26@gmail.com>
2023-07-19 06:00:06 +1200

    Assignment20

---
+++ Assignment20/Assignment20/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment20.Models;
+
+namespace Assignment20.Controllers
+{
+	public class HomeController : Controller
+	{
+		private List<CityWeather> cities = new List<CityWeather>() {
+		new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TEmperatureFahrenheit = 33},
+		new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TEmperatureFahrenheit = 60},
+		new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-1-01 9:00"), TEmperatureFahrenheit = 82}
+		};
+
+		[Route("/")]
+		public IActionResult Index()
+		{
+			return View(cities);
+		}
+
+		[Route("/weather/{cityCode?}")]
+		public IActionResult City(string? cityCode)
+		{
+			if (string.IsNullOrEmpty(cityCode))
+			{
+				return View();
+			}
+			CityWeather? city = cities.Where(temp => temp.CityUniqueCode == cityCode).FirstOrDefault();
+			return View(city);
+		}
+	}
+}
---
+++ Assignment20/Assignment20/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Assignment20.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment20.ViewComponents
+{
+	
+	public class CityViewComponent: ViewComponent
+	{
+		
+		public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+		{
+			ViewBag.CityCssClass = GetCssClassByFahrenheit(city.TEmperatureFahrenheit);
+			return View(city);
+		}
+
+		private string GetCssClassByFahrenheit(int TemperatureFahrenheit)
+		{
+			return TemperatureFahrenheit switch
+			{
+				(< 44) => "blue-back",
+				(>= 44) and (< 75) => "green-back",
+				(>= 75) => "orange-back"
+			};
+		}
+	}
+}
