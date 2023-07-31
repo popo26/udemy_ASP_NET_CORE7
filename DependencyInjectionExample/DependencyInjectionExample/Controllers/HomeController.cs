978a4f92acc03243f1e7e37f1444ba3f732bafff
popo26 <mynameispopo26@gmail.com>
2023-07-31 14:43:36 +1200

    121.Dependency Injection

---
+++ DependencyInjectionExample/DependencyInjectionExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using ServiceContracts;
+
+namespace DependencyInjectionExample.Controllers
+{
+	public class HomeController : Controller
+	{
+		//private readonly ICitiesService _citiesService;
+		
+		////constructor --- most popular than using Dependency Injection in a method
+		//public HomeController(ICitiesService citiesService)
+		//{
+		//	//create object of CitiesSErvice class
+		//	//_citiesService = new CitiesService(); //Bad practice to create object of service class directly. Instead use dependency injection.
+		//	_citiesService = citiesService;
+		//}
+
+		[Route("/")]
+		public IActionResult Index([FromServices]ICitiesService _citiesService)
+		{
+			List<string> cities = _citiesService.GetCities();
+			return View(cities);
+		}
+	}
+}
---
+++ DependencyInjectionExample/Services/CitiesService.cs
@@ -0,0 +1,25 @@
+﻿namespace Services;
+using ServiceContracts;
+
+
+public class CitiesService: ICitiesService
+{
+	private List<string> _cities;
+
+	public CitiesService()
+	{
+		_cities = new List<string>() 
+		{
+	"London",
+	"Paris",
+	"New York",
+	"Tokyo",
+	"Rome"
+	};
+	}
+
+	public List<string> GetCities()
+	{
+		return _cities;
+	}
+}
